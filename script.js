4450c04e1fcc8780b553f4ec03e647f2837857f0
Louay956 <khammassilouay7@gmail.com>
2024-07-24 00:42:48 +0100

    version2

--- script.js
+++ script.js
@@ -1,23 +1,55 @@
-const data = [
-    {
-     image:"./Images/images.jpg",
-     name:"chausure pour femme",
-     price:50,
-    },
+const shoes = [
+  { 
+   image:"./Images/images.jpg",
+   name:"chausure pour femme",
+   price:50,
+   title:"shoes"   
+  },
+  { 
+    image:"./Images/images.jpg",
+    name:"chausure pour femme",
+    price:50,
+    title:"shoes"   
+   },
+   { 
+    image:"./Images/images.jpg",
+    name:"chausure pour femme",
+    price:50,
+    title:"shoes"   
+   },
+  
+];
+const Tshirt = [
+  { 
+   image:"./Images/images.jpg",
+   name:"tshirt pour homme",
+   price:300,
+   title:"Tshirt"   
+  },
+  
+];
+const Short = [
+  { 
+   image:"./Images/images.jpg",
+   name:"tshirt pour homme",
+   price:500,
+   title:"Short"   
+  },
 ];
 
 
 const orders = {
  products: [],
- totalPrice: 1,
+ totalPrice: 0,
 };
 
 
-
-const cart = document.querySelector(".cart");
+const cart = document.querySelector(".cart")
+const commandes = document.querySelector(".commandes");
 const container = document.querySelector(".container");
 const container2 = document.querySelector(".container2");
-const totalprice = document.querySelector(".totalprice")
+const newtotalprice = document.querySelector("#newtotalprice")
+const title = document.querySelector(".title")
 
 function displayCart() {
     if (orders.totalPrice === 0) {
@@ -27,24 +59,26 @@ function displayCart() {
     }
   };
  function displayprice(){
-    totalprice.innerHTML=orders.totalPrice;
+  console.log("Displaying total price:", orders.totalPrice);
+  newtotalprice.innerHTML = orders.totalPrice;
  }
   function addToCart(item) {
     orders.products.push(item);
     orders.totalPrice += item.price;
-    totalprice.innerHTML=orders.totalPrice;
-    cart.innerHTML += `
+    displayprice();
+    commandes.innerHTML += `
         <div class="grid1">
-            <p><img src="${item.image}" alt="${item.name}"></p>
             <h1 class="price">${item.price}</h1>
             <h2 class="details">${item.name}</h2>
          </div>
            `;
     displayCart();
   };
-
-data.forEach(function (v) {
-    container.innerHTML += `
+function shoesData(T){
+  container.innerHTML ="";
+T.forEach(function (v) {
+  title.innerHTML= v.title;
+  container.innerHTML += `
            <div class="grid1">
                <p><img src="${v.image}" alt="${v.name}"></p>
                <h1 class="price">${v.price}</h1>
@@ -52,6 +86,37 @@ data.forEach(function (v) {
                <button onclick='addToCart(${JSON.stringify(v)})'>add to cart</button>
            </div>`;
   });
+  displayprice();
+};
+function TshirtData(T){
+  container.innerHTML ="";
+  T.forEach(function (v) {
+    title.innerHTML= v.title;
+    container.innerHTML += `
+             <div class="grid1">
+                 <p><img src="${v.image}" alt="${v.name}"></p>
+                 <h1 class="price">${v.price}</h1>
+                 <h2 class="details">${v.name}</h2>
+                 <button onclick='addToCart(${JSON.stringify(v)})'>add to cart</button>
+             </div>`;
+    });
+    displayprice();
+  };
+  function ShortData(T){
+    container.innerHTML ="";
+    T.forEach(function (v) {
+      title.innerHTML= v.title;
+      container.innerHTML += `
+               <div class="grid1">
+                   <p><img src="${v.image}" alt="${v.name}"></p>
+                   <h1 class="price">${v.price}</h1>
+                   <h2 class="details">${v.name}</h2>
+                   <button onclick='addToCart(${JSON.stringify(v)})'>add to cart</button>
+               </div>`;
+      });
+      displayprice();
+    };
+shoesData(shoes);
 displayCart();
 
 
